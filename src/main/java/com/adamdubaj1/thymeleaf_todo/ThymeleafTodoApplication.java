3223cef058f09775278b84741bfae6878c7671b5
adamd <adamdubaj1@gmail.com>
2023-02-25 15:17:27 +0100

    initial commit

---
+++ src/main/java/com/adamdubaj1/thymeleaf_todo/ThymeleafTodoApplication.java
@@ -0,0 +1,13 @@
+package com.adamdubaj1.thymeleaf_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTodoApplication.class, args);
+	}
+
+}
