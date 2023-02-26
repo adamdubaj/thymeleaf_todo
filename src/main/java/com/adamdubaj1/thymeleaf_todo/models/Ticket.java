26854109147b94f6ee17be72612a3bf7a24eb8ce
adamd <adamdubaj1@gmail.com>
2023-02-26 14:33:07 +0100

    add lombok

--- src/main/java/com/adamdubaj1/thymeleaf_todo/models/Ticket.java
+++ src/main/java/com/adamdubaj1/thymeleaf_todo/models/Ticket.java
@@ -1,36 +1,14 @@
 package com.adamdubaj1.thymeleaf_todo.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Ticket {
     private String name;
     private String type;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Ticket(){
-    }
-    public Ticket(String name, String type){
-        this.name = name;
-        this.type = type;
-    }
 
     @Override
     public String toString() {
